c8d5507b1cab6c197555eba2ec5e9cc93ebb39f1
zhaoqing <zhaoqing@houselux.com>
2024-11-06 16:57:01 +0800

    feat: init

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
+  <Header></Header>
   <slot />
+  <Footer></Footer>
 </template>
 
 <script setup lang="ts"></script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,23 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['nuxt-windicss', '@nuxtjs/i18n', '@pinia/nuxt', '@vueuse/nuxt'],
-  i18n: {
-    langDir: 'languages/locales',
-    locales: [
-      {
-        code: 'en-US',
-        iso: 'en-US',
-        files: ['en-US/general.json', 'en-US/error.json', 'en-US/tdk.json'],
-      },
-      {
-        code: 'zh-CN',
-        iso: 'zh-CN',
-        files: ['zh-CN/general.json', 'zh-CN/error.json', 'zh-CN/tdk.json'],
-      },
-    ],
-    strategy: 'prefix',
-    vueI18n: './languages/index',
-    defaultLocale: 'en-US',
-  },
+  modules: ['nuxt-windicss', '@pinia/nuxt', '@vueuse/nuxt'],
 })
